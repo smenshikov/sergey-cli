34c7c856791fd91e98102228d73fac452ced6d38
menshickow <menshickowserezha@yandex.ru>
2021-12-18 18:25:05 +0300

    Improved description

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log(`Hey! I name is Sergey! I am a frontend developer. I work for company 'Lucy in the Sky'`) // eslint-disable-line
+const packageJson = require('./package.json')
+
+console.log(`
+${packageJson.description}
+
+Hey! I name is Sergey! 
+I am a frontend developer. I work for company 'Lucy in the Sky'
+`)
