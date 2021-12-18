7c60f46e33e699016a13197818ff3bada49d12ba
menshickow <menshickowserezha@yandex.ru>
2021-12-18 14:34:17 +0300

    Improved description

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-console.log(`Hey! I name is Sergey! I am a frontend developer.`) // eslint-disable-line
+console.log(`Hey! I name is Sergey! I am a frontend developer. I work for company 'Lucy in the Sky'`) // eslint-disable-line
