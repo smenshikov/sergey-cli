2adc2fa67d2e4c02f61c96a4e208617068a2c604
menshickow <menshickowserezha@yandex.ru>
2021-12-20 21:03:15 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
-import chalk from 'chalk'
-import fs from 'fs'
-const packageJson = JSON.parse(fs.readFileSync('./package.json'))
+import chalk from "chalk";
+import fs from "fs";
 
-const bgYellow = chalk.bgYellowBright
-const dim = chalk.dim
-const italic = chalk.italic
-const bold = chalk.bold
+(() => {
+  const packageJson = JSON.parse(fs.readFileSync("./package.json"));
 
-console.log(`
-${bgYellow(` ${packageJson.name} `)} v${packageJson.version}
+  process.on("unhandledRejection", (err) => {
+    console.log("unhandledRejection", err); // eslint-disable-line
+  });
 
-${italic(`Hey! ${bgYellow(` My name is Sergey! `)} ${bold(`I am a frontend developer.`)}
-${dim(`I work for company 'Lucy in the Sky'`)}`)}
-`)
+  const bgYellow = chalk.bgYellowBright;
+  const dim = chalk.dim;
+  const italic = chalk.italic;
+  const bold = chalk.bold;
+
+  console.log();
+  console.log(`${bgYellow(` ${packageJson.name} `)} v${packageJson.version}`);
+  console.log(
+    `${italic(
+      `Hey! ${bgYellow(` My name is Sergey! `)} ${bold(
+        `I am a frontend developer.`
+      )}`
+    )}`
+  );
+  console.log(`${dim(`I work for company 'Lucy in the Sky'`)}`);
+  console.log();
+})();
